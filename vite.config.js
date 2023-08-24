69403bcb09ef5253ec499ef05c70b02c79131447
cvance248 <vancecaleb5@gmail.com>
2023-08-24 09:25:07 -0400

    module

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['TodoItem.css'], // Example CSS file and regular expression
+    },
+  },
 })
